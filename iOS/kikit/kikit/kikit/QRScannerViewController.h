fab132cccdfe828dbdb83cc4a9c6dedb9c58344a
Ian Barile <ian.barile@gmail.com>
2012-12-31 23:59:10 -0800

    adding files getting ready for qrcode scanning

---
+++ iOS/kikit/kikit/kikit/QRScannerViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRScannerViewController.h
+//  kikit
+//
+//  Created by Ian Barile on 12/31/12.
+//  Copyright (c) 2012 Ian Barile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRScannerViewController : UIViewController
+
+@end
---
+++ iOS/kikit/kikit/kikit/QRScannerViewController.mm
@@ -0,0 +1,38 @@
+//
+//  QRScannerViewController.m
+//  kikit
+//
+//  Created by Ian Barile on 12/31/12.
+//  Copyright (c) 2012 Ian Barile. All rights reserved.
+//
+
+#import "QRScannerViewController.h"
+
+@interface QRScannerViewController ()
+
+@end
+
+@implementation QRScannerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
