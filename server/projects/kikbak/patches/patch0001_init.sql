63d0de5607913d92b5bbf72cc85df42ab992b3da
Chen Ding <dingc05@gmail.com>
2013-07-19 02:26:31 -0700

    set default char set to latin1 and make columns' charset utf8, remove unnecessary index

--- server/projects/kikbak/patches/patch0001_init.sql
+++ server/projects/kikbak/patches/patch0001_init.sql
@@ -3,8 +3,8 @@
 CREATE TABLE `account`
 (
 	id BIGINT NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(64) NOT NULL,
-	last_name VARCHAR(64),
+	first_name VARCHAR(64) CHARACTER SET utf8 NOT NULL,
+	last_name VARCHAR(64) CHARACTER SET utf8,
 	phone_number VARCHAR(24) NOT NULL,
 	email VARCHAR(128) NOT NULL,
 	password VARCHAR(256) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE `account`
 	account_type_id SMALLINT NOT NULL,
 	create_date DATETIME NOT NULL,
 	PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `accounttype`
@@ -20,7 +20,7 @@ CREATE TABLE `accounttype`
 	id SMALLINT NOT NULL AUTO_INCREMENT,
 	type VARCHAR(64) NOT NULL UNIQUE,
 	PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 CREATE TABLE `devicetoken`
 (
@@ -31,36 +31,36 @@ CREATE TABLE `devicetoken`
     last_update_time DATETIME NOT NULL,
     last_failed_delivery DATETIME,
     PRIMARY KEY(id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `friend`
 (
 	id BIGINT(0) NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(64),
-	last_name VARCHAR(64),
+	first_name VARCHAR(64) CHARACTER SET utf8,
+	last_name VARCHAR(64) CHARACTER SET utf8,
 	facebook_id BIGINT(0),
-	username VARCHAR(128),
+	username VARCHAR(128) CHARACTER SET utf8,
 	PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `location`
 (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    address_1 VARCHAR(256) NOT NULL,
-    address_2 VARCHAR(256),
-    city VARCHAR(128) NOT NULL,
-    state VARCHAR(24) NOT NULL,
+    address_1 VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+    address_2 VARCHAR(256) CHARACTER SET utf8,
+    city VARCHAR(128) CHARACTER SET utf8 NOT NULL,
+    state VARCHAR(24) CHARACTER SET utf8 NOT NULL,
     zipcode INT NOT NULL,
-    zip_plus_four VARCHAR(4),
+    zip_plus_four VARCHAR(4) CHARACTER SET utf8,
     phone_number BIGINT NOT NULL,
     verification_code VARCHAR(8) NOT NULL,
     merchant_id BIGINT NOT NULL,
     latitude DOUBLE NOT NULL,
     longitude DOUBLE NOT NULL,
     PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `credit`
@@ -71,13 +71,13 @@ CREATE TABLE `credit`
 	user_id BIGINT NOT NULL,
 	offer_id BIGINT NOT NULL,
 	kikbak_id BIGINT NOT NULL,
-	reward_type VARCHAR (16) NOT NULL,
+	reward_type VARCHAR (16) CHARACTER SET utf8 NOT NULL,
 	begin_date DATETIME NOT NULL,
 	end_date DATETIME NOT NULL,
 	value DOUBLE NOT NULL,
     redeem_count INT DEFAULT 0,
 	PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 CREATE TABLE `allocatedgift`
 (
@@ -92,62 +92,62 @@ CREATE TABLE `allocatedgift`
     redemption_date DATETIME,
     expiration_date DATETIME NOT NULL,
     PRIMARY KEY(id)
-)ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+)ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `merchant`
 (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL,
-    description VARCHAR(4096),
+    name VARCHAR(64) CHARACTER SET utf8 NOT NULL,
+    description VARCHAR(4096) CHARACTER SET utf8,
     url VARCHAR(256),
     PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `gift`
 (
     id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
     offer_id BIGINT NOT NULL,
-    description VARCHAR(20) NOT NULL,
-    detailed_desc VARCHAR(40) NOT NULL,
+    description VARCHAR(20) CHARACTER SET utf8 NOT NULL,
+    detailed_desc VARCHAR(40) CHARACTER SET utf8 NOT NULL,
     value DOUBLE NOT NULL,
-    discount_type VARCHAR (16) NOT NULL,
-    redemption_location_type VARCHAR(16) NOT NULL,
-    validation_type VARCHAR(16) NOT NULL,
+    discount_type VARCHAR (16) CHARACTER SET utf8 NOT NULL,
+    redemption_location_type VARCHAR(16) CHARACTER SET utf8 NOT NULL,
+    validation_type VARCHAR(16) CHARACTER SET utf8 NOT NULL,
     image_url VARCHAR(256) NOT NULL,
     default_give_image_url VARCHAR(256) NOT NULL,
-    notification_text VARCHAR(200) NOT NULL,
+    notification_text VARCHAR(200) CHARACTER SET utf8 NOT NULL,
     PRIMARY KEY (id)
-)ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+)ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `kikbak`
 (
     id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
     offer_id BIGINT NOT NULL,
-    description VARCHAR(20) NOT NULL,
-    detailed_desc VARCHAR(40) NOT NULL,
+    description VARCHAR(20) CHARACTER SET utf8 NOT NULL,
+    detailed_desc VARCHAR(40) CHARACTER SET utf8 NOT NULL,
     value DOUBLE NOT NULL,
-    reward_type VARCHAR (16) NOT NULL,
-    validation_type VARCHAR(16) NOT NULL,
+    reward_type VARCHAR (16) CHARACTER SET utf8 NOT NULL,
+    validation_type VARCHAR(16) CHARACTER SET utf8 NOT NULL,
     image_url VARCHAR(256) NOT NULL,
-    notification_text VARCHAR(200) NOT NULL,
+    notification_text VARCHAR(200) CHARACTER SET utf8 NOT NULL,
     PRIMARY KEY (id)
-)ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+)ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 CREATE TABLE `offer`
 (
     id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
     merchant_id BIGINT NOT NULL,
-    name VARCHAR(64) NOT NULL,
+    name VARCHAR(64) CHARACTER SET utf8 NOT NULL,
     image_url VARCHAR(256) NOT NULL,
     tos_url VARCHAR(2048) NOT NULL,
     redeem_limit INT DEFAULT 0,
     begin_date DATETIME NOT NULL,
     end_date DATETIME NOT NULL,
     PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 CREATE TABLE `barcode`
 (
@@ -163,7 +163,7 @@ CREATE TABLE `barcode`
     begin_date DATETIME NOT NULL,
     expiration_date DATETIME NOT NULL,
     PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 create TABLE `claim`
 (
@@ -172,15 +172,15 @@ create TABLE `claim`
     kikbak_id BIGINT NOT NULL,
     offer_id BIGINT NOT NULL,
     phone_number VARCHAR(24) NOT NULL,
-    name VARCHAR(32) NOT NULL,
-    street VARCHAR(32) NOT NULL,
-    apt VARCHAR(8),
-    city VARCHAR(24) NOT NULL,
-    state VARCHAR(16) NOT NULL,
-    zipcode VARCHAR(6) NOT NULL,
+    name VARCHAR(32) CHARACTER SET utf8 NOT NULL,
+    street VARCHAR(32) CHARACTER SET utf8 NOT NULL,
+    apt VARCHAR(8) CHARACTER SET utf8,
+    city VARCHAR(24) CHARACTER SET utf8 NOT NULL,
+    state VARCHAR(16) CHARACTER SET utf8 NOT NULL,
+    zipcode VARCHAR(6) CHARACTER SET utf8 NOT NULL,
     request_date DATETIME NOT NULL,
     PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 CREATE TABLE `shared`
 (
@@ -189,15 +189,15 @@ CREATE TABLE `shared`
     merchant_id BIGINT NOT NULL,
     location_id BIGINT NOT NULL,
     offer_id BIGINT NOT NULL,
-    employee_id VARCHAR(32),
-    type VARCHAR(16) NOT NULL,
+    employee_id VARCHAR(32) CHARACTER SET utf8,
+    type VARCHAR(16) CHARACTER SET utf8 NOT NULL,
     fb_image_id BIGINT,
     image_url VARCHAR(256),
     shared_date DATETIME NOT NULL,
     referral_code VARCHAR(12) NOT NULL,
-    caption VARCHAR(1024),
+    caption VARCHAR(1024) CHARACTER SET utf8,
     PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `transaction`
@@ -214,21 +214,21 @@ CREATE TABLE `transaction`
     verification_code VARCHAR(8) NOT NULL,
     authorization_code VARCHAR(8) NOT NULL,
     PRIMARY KEY(id)
-)ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+)ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `user`
 (
 	id BIGINT NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(64),
-	last_name VARCHAR(64),
+	first_name VARCHAR(64) CHARACTER SET utf8,
+	last_name VARCHAR(64) CHARACTER SET utf8,
 	email VARCHAR(128) NOT NULL,
 	facebook_id BIGINT(0) NOT NULL UNIQUE,
 	gender TINYINT NOT NULL,
 	create_date DATETIME NOT NULL,
 	update_date DATETIME,
 	PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 CREATE TABLE `user2friend`
@@ -237,13 +237,12 @@ CREATE TABLE `user2friend`
 	user_id BIGINT NOT NULL,
 	facebook_friend_id BIGINT NOT NULL,
 	PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
 
 
 
 /* Create Indexes */
 
-CREATE INDEX email_key ON `account` (email ASC);
 CREATE INDEX email_password_key ON `account` (email ASC, password ASC);
 
 CREATE INDEX user_id_key USING BTREE ON `devicetoken` (user_id ASC);
@@ -286,11 +285,9 @@ CREATE INDEX zipcode_key ON `location` (zipcode ASC);
 
 CREATE INDEX name_key USING BTREE ON `merchant` (name ASC);
 
-CREATE INDEX merchant_id_key USING BTREE ON `offer` (merchant_id ASC);
 CREATE INDEX valid_offer_key ON `offer` (merchant_id ASC, begin_date ASC, end_date ASC);
 CREATE INDEX gift_create_key ON `offer` (begin_date ASC, end_date ASC, id ASC);
 
-CREATE INDEX user_id_key USING BTREE ON `shared` (user_id ASC);
 CREATE INDEX merchant_id_key USING BTREE ON `shared` (merchant_id ASC);
 CREATE INDEX location_id_key USING BTREE ON `shared` (location_id ASC);
 CREATE INDEX offer_id_key USING BTREE ON `shared` (offer_id ASC);
@@ -309,6 +306,5 @@ CREATE INDEX email_key USING BTREE ON `user` (email ASC);
 CREATE INDEX create_date_key ON `user` (create_date ASC);
 CREATE INDEX gender_key ON `user` (gender ASC);
 
-CREATE INDEX user_id_key USING BTREE ON `user2friend` (user_id ASC);
 CREATE INDEX facebook_id_key USING BTREE ON `user2friend` (facebook_friend_id ASC);
 create unique index compound_index on `user2friend` (user_id ASC, facebook_friend_id);
