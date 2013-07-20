dfd260af9c9900d7f7253d5e8153d31065c0389c
Maciej Bialka <maciej.bialka@gmail.com>
2013-07-20 17:42:43 +0200

    android: corrections to give screen

---
+++ android/kikbak/src/com/referredlabs/kikbak/widget/TtfEditText.java
@@ -0,0 +1,31 @@
+
+package com.referredlabs.kikbak.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import com.referredlabs.kikbak.R;
+
+public class TtfEditText extends EditText {
+
+  public TtfEditText(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    // Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+    if (isInEditMode()) {
+      return;
+    }
+
+    TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TtfTextView);
+    String fontName = styledAttrs.getString(R.styleable.TtfTextView_typeface);
+    styledAttrs.recycle();
+
+    if (fontName != null) {
+      Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+      setTypeface(typeface);
+    }
+  }
+}
