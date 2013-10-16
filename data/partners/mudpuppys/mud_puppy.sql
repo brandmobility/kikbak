030260c777edaf949639986360901ea245d4d8cf
ian Barile <ian.barile@gmail.com>
2013-10-16 00:48:03 -0700

    updating mud puppy's patch

--- data/partners/mudpuppys/mud_puppy.sql
+++ data/partners/mudpuppys/mud_puppy.sql
@@ -4,7 +4,7 @@ set @server="https://m.kikbak.me";
 #Mud Puppys
 
 insert into merchant (name,                 shortname,     description,    url,                       image_url) values 
-                     ('Mudpuppy\'s Castro', 'Mudpuppy\'s', 'pet groomer', 'http://www.mudpuppys.com', concat(@server, '/data/mud_puppy/mud_puppy_logo.png'));
+                     ('Mudpuppy\'s Castro', 'Mudpuppys', 'pet groomer', 'http://www.mudpuppys.com', concat(@server, '/data/mud_puppy/mud_puppy_logo.png'));
 
 set @id := (select LAST_INSERT_ID());
 
@@ -13,20 +13,16 @@ insert into location (address_1,            city,           state, zipcode, phon
 #                     ('Chestnut Street',   'San Francisco', 'CA',  '94114', 4154318899,   'mudpuppy',        @id,         37.800250, -122.440728),
 #                     ('#1 Isabel Street',  'Richmond',      'CA',  '94804', 5105598899,   'mudpuppy',        @id,         37.899053, -122.323972);
 
-insert into offer (merchant_id, name,        image_url,                                               tos_url,                                               begin_date, end_date) values 
-                  (@id,         'wash',      concat(@server, '/data/mud_puppy/mud_puppy_banner.png'), concat(@server, '/data/mud_puppy/mud_puppy_tos.html'), now(),      now() + interval 180 day);
+insert into offer (merchant_id, name,        image_url,                                               redeem_limit, tos_url,                                               begin_date, end_date) values 
+                  (@id,         'wash',      concat(@server, '/data/mud_puppy/mud_puppy_banner.png'), 10,           concat(@server, '/data/mud_puppy/mud_puppy_tos.html'), now(),      now() + interval 180 day);
 
 set @offer_id := (select LAST_INSERT_ID());
 
-insert into gift ( offer_id,  description, detailed_desc,                    value, discount_type, redemption_location_type, validation_type, image_url,                                             default_give_image_url) values
-                 (@offer_id, '$10 off',    'any first-time dog or cat wash', 10,   'amount',      'store',                  'qrcode',         concat(@server, '/data/mud_puppy/mud_puppy_give.png'), concat(@server, '/data/mud_puppy/mud_puppy_banner.png'));
+insert into gift ( offer_id,  description, detailed_desc,                    value, discount_type, redemption_location_type, validation_type, image_url,                                               default_give_image_url) values
+                 (@offer_id, '$10 off',    'any service for first-time customers', 10,   'amount',      'store',                  'qrcode',         concat(@server, '/data/mud_puppy/mud_puppy_give.png'), concat(@server, '/data/mud_puppy/mud_puppy_banner.png'));
 
-set @gift_id := (select LAST_INSERT_ID());                 
+set @gift_id := (select LAST_INSERT_ID());
 
 insert into kikbak (offer_id,  description,        detailed_desc,                             value, reward_type, validation_type, image_url) values
-                   (@offer_id, '$5 store credit', 'for every friend that redeems your gift' , 5.00, 'purchase',  'qrcode',         concat(@server, '/data/mud_puppy/mud_puppy_banner.png'));
+                   (@offer_id, '$5 service credit', 'for every friend that redeems your gift' , 5.00, 'purchase',  'qrcode',         concat(@server, '/data/mud_puppy/mud_puppy_banner.png'));
 
-
-
-
-	
