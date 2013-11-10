2a1b3fd7d52d5e2da7ac8a59fa82607d35b43509
Chen Ding <dingc05@gmail.com>
2013-11-09 19:31:27 -0800

    make merchant search case insensitive and fix unit test

--- server/projects/kikbak/testsrc/com/kikbak/dao/MerchantDAOTest.java
+++ server/projects/kikbak/testsrc/com/kikbak/dao/MerchantDAOTest.java
@@ -25,5 +25,14 @@ public class MerchantDAOTest extends KikbakBaseTest{
 		
 		assertEquals("23", merchant.getDescription());
 	}
+		
+	@Test
+	public void testFindMerchantByShortName(){
 	
+		Merchant merchant = roDao.findByName("pp");
+		assertEquals("23", merchant.getDescription());
+		
+		merchant = roDao.findByName("Pp");
+		assertEquals("23", merchant.getDescription());
+	}
 }
