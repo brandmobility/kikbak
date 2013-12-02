ca8382b1d1bfd3b7e9b44cf8e6768c79e55c1fcd
ian Barile <ian.barile@gmail.com>
2013-12-01 22:26:13 -0800

    updating claim submission screen on iOS for phone number only

--- iOS/kikbak/Kikbak/ViewControllers/ClaimCreditViewController.m
+++ iOS/kikbak/Kikbak/ViewControllers/ClaimCreditViewController.m
@@ -21,7 +21,7 @@ static int offsetForIOS6 = 44;
 
 @interface ClaimCreditViewController ()
 
-@property (nonatomic, strong) UIScrollView* scrollView;
+//@property (nonatomic, strong) UIScrollView* scrollView;
 @property (nonatomic, strong) UIImageView* dropShadow;
 @property (nonatomic, strong) UILabel* merchant;
 @property (nonatomic, strong) UIView* amountBackground;
@@ -30,12 +30,12 @@ static int offsetForIOS6 = 44;
 @property (nonatomic, strong) UILabel* userInfo;
 
 @property (nonatomic, strong) KikbakTextField* phoneNumber;
-@property (nonatomic, strong) KikbakTextField* name;
-@property (nonatomic, strong) KikbakTextField* street;
-@property (nonatomic, strong) KikbakTextField* apt;
-@property (nonatomic, strong) KikbakTextField* city;
-@property (nonatomic, strong) KikbakTextField* state;
-@property (nonatomic, strong) KikbakTextField* zip;
+//@property (nonatomic, strong) KikbakTextField* name;
+//@property (nonatomic, strong) KikbakTextField* street;
+//@property (nonatomic, strong) KikbakTextField* apt;
+//@property (nonatomic, strong) KikbakTextField* city;
+//@property (nonatomic, strong) KikbakTextField* state;
+//@property (nonatomic, strong) KikbakTextField* zip;
 @property (nonatomic, strong) UITextField* active;
 
 @property (nonatomic, strong) UILabel* arrival;
@@ -114,12 +114,14 @@ static int offsetForIOS6 = 44;
 
 -(void)createSubviews{
     
-    self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0,0+offsetForIOS6, self.view.frame.size.width, 500)];
-    [self.scrollView setContentSize: CGSizeMake(320, 515)];
-    self.scrollView.translatesAutoresizingMaskIntoConstraints= NO;
-    self.scrollView.scrollEnabled=YES;
-    self.scrollView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_credit_choice"]];
-    [self.view addSubview:self.scrollView];
+//    self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0,0+offsetForIOS6, self.view.frame.size.width, 500)];
+//    [self.scrollView setContentSize: CGSizeMake(320, 515)];
+//    self.scrollView.translatesAutoresizingMaskIntoConstraints= NO;
+//    self.scrollView.scrollEnabled=YES;
+//    self.scrollView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_credit_choice"]];
+//    [self.view addSubview:self.scrollView];
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_credit_choice"]];
     
     self.dropShadow = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0+offsetForIOS6, 320, 4)];
     self.dropShadow.image = [UIImage imageNamed:@"grd_navbar_drop_shadow"];
@@ -131,11 +133,11 @@ static int offsetForIOS6 = 44;
     self.merchant.textColor = UIColorFromRGB(0x3a3a3a);
     self.merchant.textAlignment = NSTextAlignmentCenter;
     self.merchant.backgroundColor = [UIColor clearColor];
-    [self.scrollView addSubview:self.merchant];
+    [self.view addSubview:self.merchant];
     
     self.amountBackground = [[UIView alloc]initWithFrame:CGRectMake(0, 54, 320, 50)];
     self.amountBackground.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_blue"]];
-    [self.scrollView addSubview:self.amountBackground];
+    [self.view addSubview:self.amountBackground];
     
     self.credit = [[UILabel alloc]initWithFrame:CGRectMake(12, 8, 100, 35)];
     self.credit.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:33];
@@ -159,59 +161,59 @@ static int offsetForIOS6 = 44;
     self.userInfo.textColor = UIColorFromRGB(0x888686);
     self.userInfo.textAlignment = NSTextAlignmentLeft;
     self.userInfo.backgroundColor = [UIColor clearColor];
-    [self.scrollView addSubview:self.userInfo];
+    [self.view addSubview:self.userInfo];
     
     self.phoneNumber = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,140,298,35)];
     self.phoneNumber.delegate = self;
     self.phoneNumber.tag = 
     self.phoneNumber.placeholder = NSLocalizedString(@"Phone Number", nil);
-    [self.scrollView addSubview:self.phoneNumber];
+    [self.view addSubview:self.phoneNumber];
     
-    self.name = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,186,298,35)];
-    self.name.delegate = self;
+//    self.name = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,186,298,35)];
+//    self.name.delegate = self;
+//    
+//    self.name.placeholder = NSLocalizedString(@"First Last Name", nil);
+//    [self.scrollView addSubview:self.name];
+//    
+//    self.street = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,232,298,35)];
+//    self.street.delegate = self;
+//    self.street.placeholder = NSLocalizedString(@"Street", nil);
+//    [self.scrollView addSubview:self.street];
+//    
+//    self.apt = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,278,298,35)];
+//    self.apt.delegate = self;
+//    self.apt.placeholder = NSLocalizedString(@"Apt", nil);
+//    [self.scrollView addSubview:self.apt];
+//    
+//    self.city = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,324,298,35)];
+//    self.city.delegate = self;
+//    self.city.placeholder = NSLocalizedString(@"City", nil);
+//    [self.scrollView addSubview:self.city];
+//    
+//    self.state = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,370,298,35)];
+//    self.state.delegate = self;
+//    self.state.placeholder = NSLocalizedString(@"State", nil);
+//    [self.scrollView addSubview:self.state];
     
-    self.name.placeholder = NSLocalizedString(@"First Last Name", nil);
-    [self.scrollView addSubview:self.name];
-    
-    self.street = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,232,298,35)];
-    self.street.delegate = self;
-    self.street.placeholder = NSLocalizedString(@"Street", nil);
-    [self.scrollView addSubview:self.street];
-    
-    self.apt = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,278,298,35)];
-    self.apt.delegate = self;
-    self.apt.placeholder = NSLocalizedString(@"Apt", nil);
-    [self.scrollView addSubview:self.apt];
-    
-    self.city = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,324,298,35)];
-    self.city.delegate = self;
-    self.city.placeholder = NSLocalizedString(@"City", nil);
-    [self.scrollView addSubview:self.city];
-    
-    self.state = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,370,298,35)];
-    self.state.delegate = self;
-    self.state.placeholder = NSLocalizedString(@"State", nil);
-    [self.scrollView addSubview:self.state];
-    
-    self.zip = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,416,298,35)];
-    self.zip.delegate = self;
-    self.zip.placeholder = NSLocalizedString(@"Zip", nil);
-    [self.scrollView addSubview:self.zip];
+//    self.zip = [[KikbakTextField alloc]initWithFrame:CGRectMake(11,416,298,35)];
+//    self.zip.delegate = self;
+//    self.zip.placeholder = NSLocalizedString(@"Zip", nil);
+//    [self.scrollView addSubview:self.zip];
     
     self.submitBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.submitBtn.frame = CGRectMake(11, 462, 298, 40);
+    self.submitBtn.frame = CGRectMake(11, self.view.frame.size.height - 120, 298, 40);
     [self.submitBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [self.submitBtn setBackgroundImage:[UIImage imageNamed:@"btn_blue"] forState:UIControlStateNormal];
     [self.submitBtn setTitle:NSLocalizedString(@"Submit", nil) forState:UIControlStateNormal];
     [self.submitBtn addTarget:self action:@selector(onSubmitBtn:) forControlEvents:UIControlEventTouchUpInside];
-    [self.scrollView addSubview:self.submitBtn];
+    [self.view addSubview:self.submitBtn];
     
 }
 
 -(void)manuallyLayoutSubviews{
-    if( ![UIDevice hasFourInchDisplay]){
-        [self.scrollView setContentSize: CGSizeMake(320, 600)];
-    }
+//    if( ![UIDevice hasFourInchDisplay]){
+//        [self.scrollView setContentSize: CGSizeMake(320, 600)];
+//    }
 }
 
 
@@ -232,21 +234,21 @@ static int offsetForIOS6 = 44;
 }
 
 -(IBAction)keyboardWillShow:(NSNotification*)notification{
-    if( ![UIDevice hasFourInchDisplay]){
-        [self.scrollView setContentSize: CGSizeMake(320, 820)];
-    }
-    else{
-        [self.scrollView setContentSize: CGSizeMake(320, 730)];
-    }
+//    if( ![UIDevice hasFourInchDisplay]){
+//        [self.scrollView setContentSize: CGSizeMake(320, 820)];
+//    }
+//    else{
+//        [self.scrollView setContentSize: CGSizeMake(320, 730)];
+//    }
 }
 
 -(IBAction)keyboardWillHide:(NSNotification*)notification{
-    if( ![UIDevice hasFourInchDisplay]){
-        [self.scrollView setContentSize: CGSizeMake(320, 560)];
-    }
-    else{
-        [self.scrollView setContentSize: CGSizeMake(320, 515)];
-    }
+//    if( ![UIDevice hasFourInchDisplay]){
+//        [self.scrollView setContentSize: CGSizeMake(320, 560)];
+//    }
+//    else{
+//        [self.scrollView setContentSize: CGSizeMake(320, 515)];
+//    }
 }
 
 -(void)showError{
@@ -256,12 +258,12 @@ static int offsetForIOS6 = 44;
 
 #pragma mark - btn actions
 -(BOOL)validInput{
-    if( self.phoneNumber.text == nil ||
-       self.name.text == nil ||
-       self.street.text == nil ||
-       self.city.text == nil ||
-       self.state.text == nil ||
-       self.zip.text == nil){
+    if( self.phoneNumber.text == nil ){// ||
+//       self.name.text == nil ||
+//       self.street.text == nil ||
+//       self.city.text == nil ||
+//       self.state.text == nil ||
+//       self.zip.text == nil){
         [self showError];
         return false;
     }
@@ -279,17 +281,17 @@ static int offsetForIOS6 = 44;
     NSMutableDictionary* dict = [[NSMutableDictionary alloc] initWithCapacity:8];
     [dict setObject:[NSNumber numberWithLong:1] forKey:@"creditId"];
     [dict setObject:self.phoneNumber.text forKey:@"phoneNumber"];
-    [dict setObject:self.name.text forKey:@"name"];
-    [dict setObject:self.street.text forKey:@"street"];
-    if( self.apt.text != nil ){
-        [dict setObject:self.apt.text forKey:@"apt"];
-    }
-    else{
-        [dict setObject:@"" forKey:@"apt"];
-    }
-    [dict setObject:self.city.text forKey:@"city"];
-    [dict setObject:self.state.text forKey:@"state"];
-    [dict setObject:self.zip.text forKey:@"zipcode"];
+//    [dict setObject:self.name.text forKey:@"name"];
+//    [dict setObject:self.street.text forKey:@"street"];
+//    if( self.apt.text != nil ){
+//        [dict setObject:self.apt.text forKey:@"apt"];
+//    }
+//    else{
+//        [dict setObject:@"" forKey:@"apt"];
+//    }
+//    [dict setObject:self.city.text forKey:@"city"];
+//    [dict setObject:self.state.text forKey:@"state"];
+//    [dict setObject:self.zip.text forKey:@"zipcode"];
     
     ClaimCreditRequest* request = [[ClaimCreditRequest alloc]init];
     [request restRequest:dict];
@@ -310,28 +312,28 @@ static int offsetForIOS6 = 44;
         base = 80;
     }
     
-    if( textField == self.name){
-        self.scrollView.contentOffset = CGPointMake(0, base);
-    }
-    if( textField == self.street){
-        self.scrollView.contentOffset = CGPointMake(0, base + 40);
-    }
-    else if( textField == self.apt){
-        self.scrollView.contentOffset = CGPointMake(0, base + 80);
-    }
-    else if(textField == self.city){
-        self.scrollView.contentOffset = CGPointMake(0, base + 120);
-    }
-    else if(textField == self.state){
-        self.scrollView.contentOffset = CGPointMake(0, base + 170);
-    }
-    else if(textField == self.zip){
-        self.scrollView.contentOffset = CGPointMake(0, base + 230);
-    }
+//    if( textField == self.name){
+//        self.scrollView.contentOffset = CGPointMake(0, base);
+//    }
+//    if( textField == self.street){
+//        self.scrollView.contentOffset = CGPointMake(0, base + 40);
+//    }
+//    else if( textField == self.apt){
+//        self.scrollView.contentOffset = CGPointMake(0, base + 80);
+//    }
+//    else if(textField == self.city){
+//        self.scrollView.contentOffset = CGPointMake(0, base + 120);
+//    }
+//    else if(textField == self.state){
+//        self.scrollView.contentOffset = CGPointMake(0, base + 170);
+//    }
+//    else if(textField == self.zip){
+//        self.scrollView.contentOffset = CGPointMake(0, base + 230);
+//    }
 }
 
 - (void)textFieldDidEndEditing:(UITextField *)textField{
-    self.scrollView.contentOffset = CGPointMake(0, 0);
+//    self.scrollView.contentOffset = CGPointMake(0, 0);
     self.active = nil;
 }
 
