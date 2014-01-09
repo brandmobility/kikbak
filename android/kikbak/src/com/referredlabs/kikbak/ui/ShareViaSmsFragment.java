42c0449022455dc40e8191fcbf125ea1fd33403b
Maciej Bialka <maciej.bialka@gmail.com>
2014-01-09 15:40:54 +0100

    android: google hangouts workaround

--- android/kikbak/src/com/referredlabs/kikbak/ui/ShareViaSmsFragment.java
+++ android/kikbak/src/com/referredlabs/kikbak/ui/ShareViaSmsFragment.java
@@ -3,10 +3,15 @@ package com.referredlabs.kikbak.ui;
 
 import java.io.IOException;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Telephony.Sms;
 
 import com.google.gson.Gson;
 import com.referredlabs.kikbak.data.ClientOfferType;
@@ -54,10 +59,40 @@ public class ShareViaSmsFragment extends ShareViaBase {
   }
 
   protected void onShareFinished(String body) {
-    Intent intent = new Intent(Intent.ACTION_VIEW);
-    intent.setData(Uri.parse("sms:"));
-    intent.putExtra("sms_body", body);
+
+    Intent intent = new Intent();
+
+    // Google Hangout is broken and does not handle sms intent correctly
+    if (isHangoutDefaultSmsApp()) {
+      intent.setPackage("com.google.android.talk");
+      intent.setAction(Intent.ACTION_SEND);
+      intent.setType("text/plain");
+      intent.putExtra(Intent.EXTRA_TEXT, body);
+    } else {
+      intent.setAction(Intent.ACTION_SENDTO);
+      intent.setData(Uri.parse("sms:"));
+      intent.putExtra("sms_body", body);
+    }
     startActivityForResult(intent, REQUEST_SEND_SMS);
+  }
+
+  @SuppressLint("NewApi")
+  boolean isHangoutDefaultSmsApp() {
+    final String hangoutPackage = "com.google.android.talk";
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      String pkg = Sms.getDefaultSmsPackage(getActivity());
+      if (hangoutPackage.equals(pkg))
+        return true;
+    }
+
+    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("sms:"));
+    PackageManager pm = getActivity().getPackageManager();
+    ResolveInfo result = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+    if (result != null && hangoutPackage.equals(result.activityInfo.packageName))
+      return true;
+
+    return false;
   }
 
   protected void onShareFailed() {
